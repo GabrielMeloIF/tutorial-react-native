222e1c7a5acc965ef3d84e2714561856b9eab174
GabrielMeloIF <melo.prado@aluno.ifsp.edu.br>
2026-02-25 13:52:09 -0300

    arrumado

--- src/components/CardUser.jsx
+++ src/components/CardUser.jsx
@@ -7,8 +7,8 @@ export default function CardUser() {
                 <Image source={{ uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFow3nU6ajG_8xpqPJWwXv1rHi3p_dT7O0sg&s'}}  style={styles.image2} />
             </View>
             <View style={styles.cardtext}>
-                <Text style={styles.name}>User Name</Text>
-                <Text style={styles.email}>User Email</Text>
+                <Text style={styles.name}>Fulano de tal</Text>
+                <Text style={styles.email}>taldefulano@email.com</Text>
             </View>
         </View>
     )
@@ -18,25 +18,34 @@ const styles = StyleSheet.create({
     card: {
         backgroundColor: '#161616',
         width: "80%",
-        height: 200,
+        height: 150,
         borderRadius: 10,
-        marginTop: 20
-    },  
+        marginTop: 20,
+        alignItems: 'center',
+        padding: 20,
+        display: 'flex',
+        flexDirection: 'row',
+        gap: 20,
+        boxShadow: '0px 6px 12px rgba(0,0,0,0.4)'
+    },
+    cardtext: {
+        gap: 10,
+    },
     image2: {
-        width: 50,
-        height: 50,
-        borderRadius: 25,
+        width: 80,
+        height: 80,
+        borderRadius: 50,
         borderColor: '#000',
     },
     name: {
         color: '#fff',
-        fontSize: 18,
+        fontSize: 25,
         fontWeight: 'bold',
         marginTop: 10
     },
     email: {
         color: '#fff',
-        fontSize: 14,
+        fontSize: 17,
         marginTop: 5
     }
 })
